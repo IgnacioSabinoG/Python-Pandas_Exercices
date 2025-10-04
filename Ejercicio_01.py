94fd544ea230b41c0093628f8848ca4c52eef1ae
IgnacioSabinoG <ignacio.sabino@yahoo.com>
2025-10-04 16:44:18 +0000

    intentos de recorrido

--- Ejercicio_01.py
+++ Ejercicio_01.py
@@ -3,6 +3,9 @@ import pandas as pd
 data = {'Nombre': ['Ignacio', 'María', 'Juan'], 'Edad': [53, 45, 13]}
 df = pd.DataFrame(data)
 
-print(df)
+print(df["Nombre"])
+
+for i in range(len(df)):
+    print(df["Nombre"][i])
 
 print(type(df))
